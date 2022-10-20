1bfef666c6e95462d6e3fed7057145427145aa27
Ihor Sharkadi <igor.sharkadi.93@gmail.com>
2022-10-20 22:43:45 +0300

    feat: token generation

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,8 +1,11 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const { User } = require("../../models/user");
 
 const { RequestError } = require("../../helpers");
+
+const { SECRET_KEY } = process.env;
 
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -15,7 +18,10 @@ const login = async (req, res) => {
   if (!passwordCompare) {
     throw RequestError(401, "Wrong password");
   }
-  const token = "321321.fdhfghlgh8fgh.jkhdfg7i";
+  const payload = {
+    id: user._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
   res.json({ token });
 };
 

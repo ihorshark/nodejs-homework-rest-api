2618ebde64611a4085a2e224d4123031ed08de40
Ihor Sharkadi <igor.sharkadi.93@gmail.com>
2022-10-26 20:37:01 +0300

    feat: Jimp added

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,5 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
+const Jimp = require("jimp");
 
 const { User } = require("../../models/user");
 
@@ -9,6 +10,15 @@ const updateAvatar = async (req, res) => {
   try {
     const { _id } = req.user;
     const { path: tempUpload, originalname } = req.file;
+
+    await Jimp.read(tempUpload)
+      .then(avatar => {
+        return avatar.resize(250, 250).write(tempUpload);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+
     const extention = originalname.split(".").pop();
     const filename = `${_id}.${extention}`;
     const resultUpload = path.join(avatarsDir, filename);
